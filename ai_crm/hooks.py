76812fcb497f9cd2e89256544fdd3cbd3141e081
gauravjain6065-maker <gauravjain6065@gmail.com>
2026-07-06 00:19:01 +0530

    o1

--- ai_crm/hooks.py
+++ ai_crm/hooks.py
@@ -60,11 +60,11 @@ app_license = "mit"
 home_page = "login"
 
 # website user home page (by Role)
-role_home_page = {
-	"System Manager": "crm-dashboard",
-	"Administrator": "crm-dashboard",
-	"All": "telecaller-performance"
-}
+# role_home_page = {
+# 	"System Manager": "crm-dashboard",
+# 	"Administrator": "crm-dashboard",
+# 	"All": "telecaller-performance"
+# }
 
 # Generators
 # ----------
