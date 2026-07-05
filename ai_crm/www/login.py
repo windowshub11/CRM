e256c170621c05ddcd9fe994b494af7eaaa7dbe3
gauravjain6065-maker <gauravjain6065@gmail.com>
2026-07-06 00:22:07 +0530

    wdjd

--- ai_crm/www/login.py
+++ ai_crm/www/login.py
@@ -4,5 +4,10 @@ from frappe.www.login import get_context as frappe_get_context
 no_cache = 1
 
 def get_context(context):
-    context = frappe_get_context(context)
+    original_user = frappe.session.user
+    frappe.session.user = "Guest"
+    try:
+        context = frappe_get_context(context)
+    finally:
+        frappe.session.user = original_user
     return context
